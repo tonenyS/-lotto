1ed9fe357d5b46828b6ad7ea818c182986cbf0b4
tonenyS <gundam7784@hotmail.com>
2020-11-17 16:17:14 +0700

    อัพใหม่จร้า ver.1.1.0

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var  number = prompt("กรอกเลขขอท่าน")
-var  logTO = Math.floor(Math.random() * 100)
+var  logTO = Math.floor(Math.random() * 1000)
 document.getElementById("youNumber").innerHTML = number;
 document.getElementById("result").innerHTML = logTO;
 
-if(number === logTO){
+if(number == logTO){
     alert("คุณถูกรางวัล")
-}else if(number !== logTO){
+    document.getElementById("id").innerHTML="ยินดีด้วยคุณถูกรางวัล"
+}else {
     alert("คุณไม่ถูกรางวัล")
+    document.getElementById("id").innerHTML="เสียใจด้วยคุณไม่ถูกรางวัล"
 }
    
